98bce2d5a4d229e22a7860396d5287eb31ea89e0
emil7809 <emil7809@gmail.com>
2021-01-07 13:38:06 +0100

    Burger menu virker på mobil

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", sidenVises);
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+    myFunction(this);
+
+}
+
+function myFunction(x) {
+
+    x.classList.toggle("change");
+}
