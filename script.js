435a4bb08c756caf825aa3cdd39bd55a3b7b6cbd
emil7809 <emil7809@gmail.com>
2021-01-09 15:59:58 +0100

    portfolio ser nice ud på mobil

--- script.js
+++ script.js
@@ -19,23 +19,3 @@ function myFunctionBurger(x) {
     console.log("myFunctionBurger");
     x.classList.toggle("change");
 }
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () {
-    myFunctionNav()
-};
-
-// Get the nav_class
-let nav_class = document.getElementById("tema_nav");
-
-// Get the offset position of the navbar
-let sticky = nav_class.offsetTop;
-
-// Add the sticky class to the nav_class when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunctionNav() {
-    if (window.pageYOffset > sticky) {
-        nav_class.classList.add("sticky");
-    } else {
-        nav_class.classList.remove("sticky");
-    }
-}
