6bafda6c506179b73199adba6843bc6cfecf0419
emil7809 <emil7809@gmail.com>
2021-01-09 04:59:33 +0100

    har tilføjet nav bar som er JS sticky, men den klikker

--- script.js
+++ script.js
@@ -11,11 +11,31 @@ function sidenVises() {
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
-    myFunction(this);
+    myFunctionBurger(this);
 
 }
 
-function myFunction(x) {
-
+function myFunctionBurger(x) {
+    console.log("myFunctionBurger");
     x.classList.toggle("change");
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunctionNav()
+};
+
+// Get the nav_class
+let nav_class = document.getElementById("tema_nav");
+
+// Get the offset position of the navbar
+let sticky = nav_class.offsetTop;
+
+// Add the sticky class to the nav_class when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunctionNav() {
+    if (window.pageYOffset > sticky) {
+        nav_class.classList.add("sticky");
+    } else {
+        nav_class.classList.remove("sticky");
+    }
 }
