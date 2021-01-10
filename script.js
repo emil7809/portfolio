68390827386a04af755e9e09cc6838c08afcbd28
emil7809 <emil7809@gmail.com>
2021-01-10 21:01:03 +0100

    kontakt virker, men validation er wack, fixer hvis jeg har tid

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ function sidenVises() {
 
     //Start emilyHoolahan animation
 
-
+    // Ved klik på kontakt element -> kontaktKlik
+    document.querySelector("#kontakt").addEventListener("click", kontaktKlik);
 
 
 }
@@ -38,6 +39,14 @@ function kontaktKlik() {
     //Start contactKlik animation
 
     //Åben kontakt
+    document.querySelector("#kontakt_siden_container").classList.remove("hidden_kontakt");
+    document.querySelector("#besked_sendt").classList.add("hidden_kontakt");
+
+    // Ved klik på x element -> x
+    document.querySelector("#x").addEventListener("click", x);
+
+    // Ved klik på send element -> sendBeskedKlik
+    document.querySelector("#sendBtn").addEventListener("click", sendBeskedKlik)
 
 }
 
@@ -46,8 +55,51 @@ function sendBeskedKlik() {
 
     //Start sendBeskedKlik animation
 
-    //luk kontakt
-
     //Åben messageSendt
+    document.querySelector("#besked_sendt").classList.remove("hidden_kontakt");
+
+    // Ved klik på x2 element -> x2
+    document.querySelector("#x2").addEventListener("click", x2);
+
+}
+
+function x() {
+    console.log("x");
+
+    //Start x animation
+
+
+    //Start luk animation
+
+
+    //Luk siden ned
+    document.querySelector("#kontakt_siden_container").classList.add("hidden_kontakt");
+    document.querySelector("#besked_sendt").classList.add("hidden_kontakt");
+
+}
+
+function x2() {
+    console.log("x2");
+
+    //Start x animation
+
+
+    //Start luk animation
+
+
+    //Luk siden ned
+    document.querySelector("#kontakt_siden_container").classList.add("hidden_kontakt");
+
+}
+
+function tilbage() {
+    console.log("tilbage");
+
+    //Start back animation
+
+
+    //Luk siden ned
+
+
 
 }
